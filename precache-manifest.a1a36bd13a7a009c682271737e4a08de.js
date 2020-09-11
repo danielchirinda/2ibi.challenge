8538c3b54082c523bdb34700543316e94ba21a7f
dchirinda <daniel.chirinda@cedsif.gov.mz>
2020-09-11 12:22:10 +0200

    Updates

---
+++ precache-manifest.a1a36bd13a7a009c682271737e4a08de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a87d019223a928fc880f5c86ebf640d",
+    "url": "/2ibi.challenge/index.html"
+  },
+  {
+    "revision": "37b9284e810d8b6b4b01",
+    "url": "/2ibi.challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "963ad6545638ee5c93d5",
+    "url": "/2ibi.challenge/static/js/2.c7d60968.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/2ibi.challenge/static/js/2.c7d60968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b9284e810d8b6b4b01",
+    "url": "/2ibi.challenge/static/js/main.a6942759.chunk.js"
+  },
+  {
+    "revision": "9d4d129bddfd1675127e",
+    "url": "/2ibi.challenge/static/js/runtime-main.f3b15d13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2ibi.challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
